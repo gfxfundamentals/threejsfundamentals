359bc738bd3e4c369467bb95610e4db83fd87068
greggman <github@greggman.com>
2021-02-06 12:09:05 +0000

    Deploying to gh-pages from  @ 9cd94f471ed16320b5c27554ef05471696a7c092 🚀

--- threejs/lessons/resources/threejs-lights.js
+++ threejs/lessons/resources/threejs-lights.js
@@ -1,5 +1,5 @@
-import * as THREE from '../../resources/threejs/r122/build/three.module.js';
-import {OrbitControls} from '../../resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
+import * as THREE from '../../resources/threejs/r125/build/three.module.js';
+import {OrbitControls} from '../../resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
 import {threejsLessonUtils} from './threejs-lesson-utils.js';
 
 {
@@ -21,17 +21,17 @@ import {threejsLessonUtils} from './threejs-lesson-utils.js';
   const makeScene = function() {
 
     const cubeSize = 4;
-    const cubeGeo = new THREE.BoxBufferGeometry(cubeSize, cubeSize, cubeSize);
+    const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
     const cubeMat = new THREE.MeshPhongMaterial({color: '#8AC'});
 
     const sphereRadius = 3;
     const sphereWidthDivisions = 32;
     const sphereHeightDivisions = 16;
-    const sphereGeo = new THREE.SphereBufferGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
+    const sphereGeo = new THREE.SphereGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
     const sphereMat = new THREE.MeshPhongMaterial({color: '#CA8'});
 
     const planeSize = 40;
-    const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
     const planeMat = new THREE.MeshPhongMaterial({
       map: makeCheckerTexture(planeSize),
       side: THREE.DoubleSide,
