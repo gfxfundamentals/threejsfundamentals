4fe69baec0e11585843a19a31085113813b846cd
Gregg Tavares <gman@chromium.org>
2021-10-08 15:06:38 -0700

    wip

--- threejs/lessons/resources/lesson.js
+++ threejs/lessons/resources/lesson.js
@@ -2,40 +2,13 @@
 /* eslint-disable strict */
 'use strict';  // eslint-disable-line
 
-/* global jQuery, settings, contributors */
+/* global jQuery */
 
 (function($){
 
 function getQueryParams() {
   return Object.fromEntries(new URLSearchParams(window.location.search).entries());
 }
-
-//
-function replaceParams(str, subs) {
-  return str.replace(/\${(\w+)}/g, function(m, key) {
-    return subs[key];
-  });
-}
-
-function showContributors() {
-  // contribTemplate: 'Thank you
-  // <a href="${html_url}">
-  // <img src="${avatar_url}">${login}<a/>
-  //  for <a href="https://github.com/${owner}/${repo}/commits?author=${login}">${contributions} contributions</a>',
-  try {
-    const subs = {...settings, ...contributors[Math.random() * contributors.length | 0]};
-    const template = settings.contribTemplate;
-    const html = replaceParams(template, subs);
-    const parent = document.querySelector('#forkongithub>div');
-    const div = document.createElement('div');
-    div.className = 'contributors';
-    div.innerHTML = html;
-    parent.appendChild(div);
-  } catch (e) {
-    console.error(e);
-  }
-}
-showContributors();
 
 $(document).ready(function($){
   if (window.prettyPrint) {
